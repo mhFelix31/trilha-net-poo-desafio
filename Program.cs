affd1658a8b61a368972060448bd89f0e7a8ab52
Felix <mhfelix31@gmail.com>
2024-04-14 00:31:54 -0300

    feat: added 'test' in Program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123-456", "Model One", "1111111", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone iphone = new Iphone("123-457", "Iphone 15", "1111112", 256);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
